b3f7019164ad8a1440022874c7cde8c37578e875
lucasmelo59 <lucas-melo59@hotmail.com>
2022-07-08 17:02:05 -0300

    Crud

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule
+
+  ]
+})
+export class CoursesModule { }
---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    {_id: '1', name: 'Angulr', category: 'front-end'}
+  ];
+  displayedColumns= ['name','category'];
+
+  constructor() {
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
